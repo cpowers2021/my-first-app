c7507e4817ac41ea7f43f7c7d552f74840721b8f
BuildTools <unconfigured@null.spigotmc.org>
2020-11-01 14:32:41 -0800

    Feat: Add a green/red background when online/offline with Math.random() determining offline/online status and make text white when online

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Enter your server name here!';
+  serverCreated = false;
   constructor() {
   setTimeout(() => {
     this.allowNewServer = true;
@@ -21,6 +22,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created. The name of the server is ' + this.serverName;
   }
 
